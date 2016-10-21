9545f258a181b622bf509f112589fe8b14e50dc0
Peyman Khanjan <p.khanjan@gmail.com>
2016-10-21 12:00:52 +0100

    Added Kochava header files.

---
+++ Kochava/Kochava.h
@@ -0,0 +1,20 @@
+//
+//  Kochava.h
+//  Kochava
+//
+//  Created by Peyman Khanjan on 21/10/2016.
+//  Copyright © 2016 Snupps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kochava.
+FOUNDATION_EXPORT double KochavaVersionNumber;
+
+//! Project version string for Kochava.
+FOUNDATION_EXPORT const unsigned char KochavaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kochava/PublicHeader.h>
+
+
+#import <Kochava/TrackAndAd.h>
