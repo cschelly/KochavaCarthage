c38244641377e2b19d441bb708758e592e1be1e1
Peyman Khanjan <p.khanjan@gmail.com>
2016-10-21 14:35:24 +0100

    Added the dummy Kochava m file.

---
+++ Kochava/Kochava.m
@@ -0,0 +1,12 @@
+//
+//  Kochava.m
+//  Kochava
+//
+//  Created by Peyman Khanjan on 21/10/2016.
+//  Copyright © 2016 Snupps. All rights reserved.
+//
+
+#import "TrackAndAd.h"
+
+@interface KochavaTracker (Dummy) @end
+@implementation KochavaTracker (Dummy) @end
